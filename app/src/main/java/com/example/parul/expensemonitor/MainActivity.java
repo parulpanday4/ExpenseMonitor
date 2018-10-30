95c9a20572fcf28b236e4489b138f2c3fe0919b2
Parul <parulpanday4@outlook.com>
2018-10-30 15:45:47 +0530

    Date picker added Fab added
    Changed numeric month to alphabetic month names.

--- app/src/main/java/com/example/parul/expensemonitor/MainActivity.java
+++ app/src/main/java/com/example/parul/expensemonitor/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.parul.expensemonitor;
 
 import android.app.DatePickerDialog;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -42,19 +41,23 @@ public class MainActivity extends AppCompatActivity
     {
         if (item.getItemId() == R.id.menu_showByDate) // this is for clicked item -> If there are more than one menu then we will distinguish it by using if else if
         {
-            final Calendar calendar = Calendar.getInstance();   //object creation
-            new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
-                {
-                    calendar.set(year,month,dayOfMonth);
-                    Toast.makeText(MainActivity.this, dayOfMonth+"/"+month+1+"/"+year, Toast.LENGTH_SHORT).show();
-                }
-            },Calendar.YEAR,Calendar.MONTH,Calendar.DAY_OF_MONTH).show();
-
-
+               getDate();
         }
         return super.onOptionsItemSelected(item);
+    }
+    void getDate(){
+        final Calendar calendar = Calendar.getInstance();   //object creation
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
+            {
+                String monthNames[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+                calendar.set(year,month,dayOfMonth);
+                Toast.makeText(MainActivity.this, dayOfMonth+"/"+monthNames[month]+"/"+year, Toast.LENGTH_SHORT).show();
+            }
+        },calendar.get(Calendar.YEAR)
+                ,calendar.get(Calendar.MONTH)
+                ,calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
 
